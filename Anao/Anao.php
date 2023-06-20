cdd2cb9388851310f8f700bb3b9f00cffb4aceb2
Kaique Santos <kaique30122004@gmail.com>
2023-06-20 17:15:48 -0300

    create e delete funcionando

--- Anao/Anao.php
+++ Anao/Anao.php
@@ -2,11 +2,11 @@
 
 class Anao
 {
-    private string $nome;
-    private string $tipo;
-    private int $idade;
-    private float $preco;
-    private bool $isVendido;
+    public string $nome;
+    public string $tipo;
+    public int $idade;
+    public float $preco;
+    public bool $isVendido;
 
     public function __construct(string $nome, string $tipo, int $idade, float $preco = 0, bool $isVendido = false)
     {
@@ -14,57 +14,6 @@ class Anao
         $this->tipo = $tipo;
         $this->idade = $idade;
         $this->preco = $preco;
-        $this->isVendido = $isVendido;
-    }
-
-
-    public function getNome(): string
-    {
-        return $this->nome;
-    }
-
-    public function setNome(string $nome): void
-    {
-        $this->nome = $nome;
-    }
-
-    public function getTipo(): string
-    {
-        return $this->tipo;
-    }
-
-    public function setTipo(string $tipo): void
-    {
-        $this->tipo = $tipo;
-    }
-
-    public function getIdade(): int
-    {
-        return $this->idade;
-    }
-
-    public function setIdade(int $idade): void
-    {
-        $this->idade = $idade;
-    }
-
-    public function getPreco(): float
-    {
-        return $this->preco;
-    }
-
-    public function setPreco(float $preco): void
-    {
-        $this->preco = $preco;
-    }
-
-    public function isVendido(): bool
-    {
-        return $this->isVendido;
-    }
-
-    public function setVendido(bool $isVendido): void
-    {
         $this->isVendido = $isVendido;
     }
 }
--- Anao/Connection.php
+++ Anao/Connection.php
@@ -12,6 +12,8 @@ if ($argc != 2) {
 //CONSTANTES E FUNÇÕES AUXILIARES
 $message = '';
 $url = $argv[1];
+$urlRestart = $argv[1];
+
 $urls = GET_URLS($url);
 
 function options()
@@ -26,14 +28,20 @@ function options()
     return $options;
 }
 
+function restartUrl(){
+    global $url;
+    global $urlRestart;
+    $url = $urlRestart;
+}
+
 function hasMessage($value, $situation = 1)
 {
+    global $url;
     global $message;
     if (strlen($message) > 0 && $situation == 1) {
         $message = "Aviso: $value\n\n";
     } elseif ($situation == 0) {
-        $message = "Resultado:\n $value\n
-        \n----------------------------------------------\n";
+        $message = "Resultado:\n$value\n\n----------------------------------------------\n";
     }
     return true;
 }
@@ -49,17 +57,18 @@ function menu()
 
     echo "\n=============== SISTEMA DE VENDA DE ANÕES ===============\n\n";
     foreach (options() as $key => $value) {
-        echo $key . " - " . $value . "\n";
+        echo $key." - " . $value . "\n";
     }
-    echo "\n----------------------------------------------\n";
+    echo "----------------------------------------------\n";
     echo "Escolha: ";
-    $escolha = trim(readline());
-    return (int)$escolha;
+    $escolha = (int)trim(readline());
+    return $escolha;
 }
 
 //REDIRECIONAR ESCOLHA
 do {
-    switch (menu()) {
+    $choice = menu();
+    switch ($choice) {
         case 1:
             GET("baldes");
             break;
@@ -81,14 +90,12 @@ do {
             hasMessage("Escolha uma opção disponível!");
             break;
     }
-} while (menu() != 0);
+} while ($choice != 0);
 
 //FUNÇÕES PRINCIPAIS
 function create()
 {
-    global $url;
-    global $urls;
-
+    restartUrl();
     echo "\033[H\033[2J";
     echo "Iniciando sistema de cadastro de anões\n\n";
 
@@ -104,16 +111,11 @@ function create()
     $anao = new Anao($name, $type, $age, $price);
 
     $res = request(
-        _url('balde', ['{balde}']),
+        _url('balde', [['{balde}',$name]]),
         [
-            [CURLOPT_CUSTOMREQUEST, 'POST'],
+            [CURLOPT_CUSTOMREQUEST, 'PUT'],
             [CURLOPT_HTTPHEADER, array('Content-Type: application/json')],
-            [
-                CURLOPT_POSTFIELDS, json_encode([
-                    'usuario' => $name,
-                    'valor' => $anao
-                ])
-            ]
+            [CURLOPT_POSTFIELDS, json_encode(['usuario' => "$name"])]
         ]
     );
 
@@ -122,8 +124,47 @@ function create()
     } else {
         hasMessage($res["codigo"], 1);
     }
+
+    restartUrl();
+
+    sendData($name,$anao);
 }
 
+function delete() {
+    global $message;
+
+    restartUrl();
+    echo "\033[H\033[2J";
+    echo "Iniciando sistema de deletar anões\n\n";
+    GET("baldes");
+
+    echo "\n".$message;
+
+    echo "Escolha qual o anão que deseja deletar: ";
+    $choice = (string)readline();
+    $delete = '';
+
+    foreach (GET("baldes") as $value) {
+        if ($value["id"]==$choice) {
+            $delete = $value;
+        }
+    }
+
+    $res = request(
+        _url('objeto', [['{balde}',$delete["nome"]], ['{chave}', $delete["id"]]]),
+        [[CURLOPT_CUSTOMREQUEST, 'DELETE']]
+    );
+
+    restartUrl();
+
+    sendDelete($value);
+
+    if ($res["codigo"] == 200) {
+        hasMessage("O anão \"". $value["nome"] ."\" foi deletado com sucesso", 0);
+    } else {
+        hasMessage($res["codigo"], 1);
+    }
+}
 
 //FUNÇÕES DE REQUISIÇÃO
 function GET_URLS($url)
@@ -135,17 +176,49 @@ function GET_URLS($url)
 
 function GET($scope)
 {
+    restartUrl();
     global $url;
     global $urls;
     $res = request($url . $urls[$scope]);
 
     if ($res['codigo'] == 200) {
-        $data = json_decode($res['corpo'], true);
-        hasMessage($data, 0);
+        $dataJson = json_decode($res['corpo'], true);
+        $string = '';
+        foreach ($dataJson as $value) {
+            $string .=$value["id"].' - '.$value["nome"]."\n";
+            $data[] = ["id"=>$value["id"],"nome"=>$value["nome"]];
+        }
+        hasMessage($string, 0);
     } else {
-        hasMessage("Erro ao obter os dados dos anões.\n
-        Erro {$res['codigo']}: {$res['corpo']}\n");
+        hasMessage("Erro ao obter os dados dos anões.\nErro: {$res['codigo']}: {$res['corpo']}\n");
     }
+    return $data;
+}
+
+function sendData($key,$object){
+    $res = request(
+        _url('balde', [['{balde}',$key]]),
+        [
+            [CURLOPT_CUSTOMREQUEST, 'POST'],
+            [CURLOPT_HTTPHEADER, array('Content-Type: application/json')],
+            [CURLOPT_POSTFIELDS, json_encode(['usuario' => "$key", 'valor' => json_encode($object)])]
+        ]
+    );
+
+    if ($res["codigo"] != 201) {
+        hasMessage("\nSendData: ".$res["codigo"], 1);
+    } 
+}
+
+function sendDelete($value){
+    $res = request(
+        _url('balde', [['{balde}',$value["nome"]]]),
+        [[CURLOPT_CUSTOMREQUEST, 'DELETE']]
+    );
+
+    if ($res["codigo"] != 200) {
+        hasMessage("\nSendDelete: ".$res["codigo"], 1);
+    } 
 }
 
 function _url(string $url_key, array $substituicoes = [])
